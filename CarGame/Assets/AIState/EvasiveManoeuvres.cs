95480e14f2ab0f4a710c18f67d1fbb146b519538
Stephen <clarksj4@student.op.ac.nz>
2016-10-05 18:20:41 +1300

    Add AIStates

---
+++ CarGame/Assets/AIState/EvasiveManoeuvres.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarGame
+{
+    public class EvasiveManoeuvres : AIState
+    {
+        public EvasiveManoeuvres(AIBattleMode ai, CarDriving car)
+            : base(ai, car) { /* Nothing */ }
+    }
+}
---
+++ CarGame/Assets/AIState/HuntEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarGame
+{
+    public class HuntEnemy : AIState
+    {
+        public HuntEnemy(AIBattleMode ai, CarDriving car)
+            : base(ai, car) { /* Nothing */ }
+    }
+}
