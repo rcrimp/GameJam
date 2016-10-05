f6b8ecf8a67b345baecb6ae5432c4795415c0856
StephGarland <garlasl1@student.op.ac.nz>
2016-10-06 11:11:51 +1300

    Speedboost powerup and test environment

---
+++ CarGame/Assets/PowerUps/PowerupSpawner.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupSpawner : MonoBehaviour {
+
+    public int MaxNumberOfPowerups;
+    public GameObject powerUpPrefab;
+    public GameObject track;
+    Bounds trackBounds;
+
+	// Use this for initialization
+	void Start () {
+
+        trackBounds = track.GetComponent<Renderer>().bounds;
+        SpawnPowerUps(MaxNumberOfPowerups);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SpawnPowerUps(int nPowerUps)
+    {
+        for (int i = 0; i < nPowerUps; i++)
+        {
+            CreateRandomPowerup();
+        }        
+    }
+
+    public void CreateRandomPowerup()
+    {
+        //create powerup prefab at random location within bounds of the track
+        float x = trackBounds.center.x + Random.Range(-trackBounds.extents.x, trackBounds.extents.x);
+        float z = trackBounds.center.z + Random.Range(-trackBounds.extents.z, trackBounds.extents.z);
+        GameObject powerUp = (GameObject)Instantiate(powerUpPrefab, new Vector3(x, 0.5f, z), Quaternion.identity);
+
+        //give the prefab a random power
+        int rand = Random.Range(0, 3);
+        switch (rand)
+        {
+            case 0:
+                CreateSpeedBoost(powerUp);
+                break;
+            default:
+                Debug.Log("Creating an exciting new powerup");
+                break;
+        }
+
+
+    }
+
+    public void CreateSpeedBoost(GameObject powerUp)
+    {
+        powerUp.AddComponent<SpeedBoost>();
+        SpeedBoost speedBoost = powerUp.GetComponent<SpeedBoost>();
+        speedBoost.boostDuration = 0.5f;
+        speedBoost.speedIncrease = 10;
+        speedBoost.slowDownDuration = 2;
+        Debug.Log("SpeedBoost");
+    }
+}
---
+++ CarGame/Assets/PowerUps/SpeedBoost.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoost : MonoBehaviour
+{
+
+    public float boostDuration;
+    public float speedIncrease;
+    public float slowDownDuration;
+    private float originalSpeed;   
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("THERE'S A COLLISION");
+        //increase the speed of the thing that collides with me
+
+        //saves a ref to the behaviour script of the collider. if there isn't one, value is null
+        BurgerCarBehaviour collidingObject = collider.gameObject.GetComponent<BurgerCarBehaviour>();
+
+        //if there's a behaviourScript
+        if (collidingObject != null)
+        {
+            Debug.Log("THERE'S A BEHAVIOUR SCRIPT");
+            originalSpeed = collidingObject.Speed;
+            StartCoroutine(Boost(collidingObject));
+        }
+    }
+
+    public IEnumerator Boost(BurgerCarBehaviour collidingObject)
+    {
+        collidingObject.Speed *= speedIncrease;
+        Debug.Log("BOOST");
+        yield return new WaitForSeconds(boostDuration);
+        StartCoroutine(SlowDown(collidingObject));
+    }
+
+    public IEnumerator SlowDown(BurgerCarBehaviour collidingObject)
+    {
+        float expiredDuration = 0;
+        while(expiredDuration < slowDownDuration)
+        {
+            expiredDuration += Time.deltaTime;
+            collidingObject.Speed = Mathf.Lerp(collidingObject.Speed, originalSpeed, expiredDuration/slowDownDuration);
+            yield return null;
+        }
+        collidingObject.Speed = originalSpeed;
+    }
+}
