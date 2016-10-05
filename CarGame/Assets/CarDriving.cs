2c0e487a2d5fa997bd9127aa5dd686029d04346c
Reuben Crimp <reubencrimp@gmail.com>
2016-10-05 14:31:40 +1300

    Made a shitty car interface

--- CarGame/Assets/CarDriving.cs
+++ CarGame/Assets/CarDriving.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class CarDriving : MonoBehaviour {
 
+    private float speed;
+    private Vector3 direction;
+
+    public void Brake() {
+        speed = 0;
+    }
+
+    public void Accelerate() {
+        speed = 2;
+    }
+
+    public void SteerLeft() {
+        direction = Quaternion.AngleAxis(3, Vector3.up) * direction;
+    }
+
+    public void SteerRight() {
+        direction = Quaternion.AngleAxis(-3, Vector3.up) * direction;
+    }
+
 	// Use this for initialization
 	void Start () {
-	
+        speed = 0;
+        direction = new Vector3(0, 0, 1); // z forward
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Vector3 pos = transform.position;
+        pos += direction * (speed * Time.deltaTime);
+
+        transform.rotation = Quaternion.LookRotation(direction.normalized);
+
+        transform.position = pos;
 	}
 }
---
+++ CarGame/Assets/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    CarDriving car = null;
+
+	// Use this for initialization
+	void Start () {
+        car = GetComponentInChildren<CarDriving>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (car != null) {
+            // get input
+            float steer = Input.GetAxis("Horizontal");
+            float accelerate = Input.GetAxis("Vertical");
+
+            // call car controller methods
+            if (steer > 0) car.SteerLeft();
+            if (steer < 0) car.SteerRight();
+            if (accelerate > 0) car.Accelerate();
+            if (accelerate < 0) car.Brake();
+        }
+	}
+}
