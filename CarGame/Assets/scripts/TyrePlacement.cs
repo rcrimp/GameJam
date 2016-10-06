501c673a1de38d339cec1db88fd80f5b67f5fe37
Reuben Crimp <reubencrimp@gmail.com>
2016-10-07 12:01:14 +1300

    who the fuck knows

--- CarGame/Assets/scripts/TyrePlacement.cs
+++ CarGame/Assets/scripts/TyrePlacement.cs
@@ -12,31 +12,26 @@ public class TyrePlacement : MonoBehaviour {
     void UpdateWheelHeight(Transform wheelTransform, WheelCollider collider) {
         Vector3 localPosition = wheelTransform.localPosition;
         WheelHit hit = new WheelHit();
+        
         // see if we have contact with ground
         if (collider.GetGroundHit(out hit)) {
             float hitY = collider.transform.InverseTransformPoint(hit.point).y;
             localPosition.y = hitY + collider.radius;
 
-            if (wheelCollider.GetComponent<ParticleSystem>() != null)
-            {
+            if (wheelCollider.GetComponent<ParticleSystem>() != null) {
                 ParticleSystem.EmissionModule em = wheelCollider.GetComponent<ParticleSystem>().emission;
-
-                em.enabled = true;
-                if (
-                    //Mathf.Abs(hit.forwardSlip) >= wheelCollider.forwardFriction.extremumSlip ||
-                        Mathf.Abs(hit.sidewaysSlip) >= wheelCollider.sidewaysFriction.extremumSlip
-                    )
-                {
-                    em.enabled = true;
-                }
-                else
-                {
-                    em.enabled = false;
-                }
+                em.enabled =
+                        //Mathf.Abs(hit.forwardSlip) >= wheelCollider.forwardFriction.extremumSlip ||
+                        Mathf.Abs(hit.sidewaysSlip) >= wheelCollider.sidewaysFriction.extremumSlip;
             }
         } else {
             // no contact with ground, just extend wheel position with suspension distance
             localPosition = Vector3.Lerp(localPosition, -Vector3.up * collider.suspensionDistance, .05f);
+            if (wheelCollider.GetComponent<ParticleSystem>() != null)
+            {
+                ParticleSystem.EmissionModule em = wheelCollider.GetComponent<ParticleSystem>().emission;
+                em.enabled = false;
+            }
         }
         wheelTransform.localPosition = localPosition;
         wheelAngle += (((collider.rpm * 360f) / 60f) * Time.fixedDeltaTime) % 360f;
