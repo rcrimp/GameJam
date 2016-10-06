72f818c9ab60eef9d9890e437a5304645037a2b0
Sam-Bates <batez1993@gmail.com>
2016-10-06 20:37:26 +1300

    mrege fix v2

--- CarGame/Assets/scripts/PlayerControls.cs
+++ CarGame/Assets/scripts/PlayerControls.cs
@@ -10,23 +10,11 @@ public class PlayerControls : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         car = GetComponent<CarController>();
-<<<<<<< HEAD
-        gameObject.SetActive(true);
-        /*if (multiplayer && !isLocalPlayer)
-        {
-            cameraObject.SetActive(false);
-        }*/
-=======
->>>>>>> 9b29720a91d58f8f251cbf62d39f1f4a78caaf60
     }
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-        if (true){ //car != null && (!multiplayer || isLocalPlayer)) {
-=======
         if (car != null) {
->>>>>>> 9b29720a91d58f8f251cbf62d39f1f4a78caaf60
             float steer = Input.GetAxis("Horizontal");
             float gas = Input.GetAxis("Vertical");
             car.updateInput(steer, gas);
