0794a249ca77bbf8fcfabf271f750ca870b26012
StephGarland <garlasl1@student.op.ac.nz>
2016-10-07 14:38:27 +1300

    Missile collision animation

--- CarGame/Assets/PowerUps/Explosion.cs
+++ CarGame/Assets/PowerUps/Explosion.cs
@@ -5,39 +5,54 @@ public class Explosion : MonoBehaviour
 {
     public float radius = 2;
     public float force = 10;
+    public int durationInSeconds = 1;
 
     void Start()
     {
         Collider[] impacted = Physics.OverlapSphere(transform.position, radius);
         Debug.Log(impacted.Length);
+
+        //for each thing in the explosion radius, explode it (if it has a rigidbody)
         for (int i = 0; i < impacted.Length; i++)
         {
             Debug.Log(impacted[i].name);
             if(impacted[i].attachedRigidbody != null)
                 impacted[i].attachedRigidbody.AddExplosionForce(force, transform.position, radius);
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
-
-    public void DustCloudAnimation()
-    {
+        //set random rotation for each child in the dustcloud prefab
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform cloudPart = transform.GetChild(i).transform;
-            cloudPart.rotation = Random.rotation;//randomise it's rotation,
-
-            //scale it up to a random size,
-            //Vector3 randomSize = 
-                //new Vector3(Random.Range(cubeSizeWidthMin, cubesizeWidthMax), 1, 1);
-            
-            //cloudPart.localScale += Vector3.one * scaleRate;
-            //scale it back down
-
+            cloudPart.rotation = Random.rotation;
         }
+
+            StartCoroutine("DustCloudAnimation");
+    }
+
+    public IEnumerator DustCloudAnimation()
+    {
+        float elapsedDuration = 0;
+        while(elapsedDuration < durationInSeconds)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform cloudPart = transform.GetChild(i).transform;
+                cloudPart.localScale *= 1.04f;//make it grow
+
+                //fade
+                float t = elapsedDuration / durationInSeconds;
+                float alpha = Mathf.Lerp(0.2f, 0, t);
+
+                var material = transform.GetChild(i).GetComponent<Renderer>().material;
+                var color = material.color;
+                material.color = new Color(color.r, color.g, color.b, alpha);
+
+            }
+            elapsedDuration += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 
 }
