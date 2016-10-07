043ae882924d270213382c5f15c37a29e498452c
Sam-Bates <batez1993@gmail.com>
2016-10-07 14:51:03 +1300

    Created house arena

---
+++ CarGame/Assets/CarSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSound : MonoBehaviour {
+
+    AudioSource audio;
+    Vector3 lastPosition = Vector3.zero;
+    public float speed;
+
+    int startingPitch = 2;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.pitch = startingPitch;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        speed = (transform.position - lastPosition).magnitude;
+        lastPosition = transform.position;
+
+        audio.pitch = speed;
+	}
+}
---
+++ CarGame/Assets/scripts/CarColliderScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarColliderScript : MonoBehaviour {
+
+    public Collider next = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider car)
+    {
+        Debug.Log(car.gameObject.name + "Just entered the box collider ");
+
+        car.GetComponent<AIControls>().Follow(next.transform);//.target = next.transform;
+    }
+}
